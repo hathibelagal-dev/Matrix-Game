ec449d4cf0f27fb2067a3c4ab9bb0496f922d438
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-08-15 04:23:41 +0530

    turn around

--- Matrix-Game-2/utils/conditions.py
+++ Matrix-Game-2/utils/conditions.py
@@ -2,9 +2,11 @@
 import torch
 import random
 
-def combine_data(data, num_frames=57, keyboard_dim=6):
+def combine_data(data, num_frames=57, keyboard_dim=6, mouse=True):
     assert num_frames % 4 == 1
     keyboard_condition = torch.zeros((num_frames, keyboard_dim))
+    if mouse == True:
+        mouse_condition = torch.zeros((num_frames, 2))
     
     current_frame = 0
     selections = [12]
@@ -13,38 +15,67 @@ def combine_data(data, num_frames=57, keyboard_dim=6):
         rd_frame = selections[random.randint(0, len(selections) - 1)]
         rd = random.randint(0, len(data) - 1)
         k = data[rd]['keyboard_condition']
+        if mouse == True:
+            m = data[rd]['mouse_condition']
         
         if current_frame == 0:
             keyboard_condition[:1] = k[:1]
+            if mouse == True:
+                mouse_condition[:1] = m[:1]
             current_frame = 1
         else:
             rd_frame = min(rd_frame, num_frames - current_frame)
             repeat_time = rd_frame // 4
             keyboard_condition[current_frame:current_frame+rd_frame] = k.repeat(repeat_time, 1)
+            if mouse == True:
+                mouse_condition[current_frame:current_frame+rd_frame] = m.repeat(repeat_time, 1)
             current_frame += rd_frame
+    if mouse == True:
+        return {
+                "keyboard_condition": keyboard_condition,
+                "mouse_condition": mouse_condition
+            }
     return {"keyboard_condition": keyboard_condition}
 
 def Bench_actions_universal(num_frames, num_samples_per_action=4):
     actions_single_action = [
-        "forward",        
+             
     ]
-    actions_to_test = actions_single_action * 1
+    actions_single_camera = [   
+        "camera_l",
+    ]
+    actions_to_test = actions_single_action * 1 + actions_single_camera * 1
 
-    base_action = actions_single_action
+    base_action = actions_single_action + actions_single_camera
 
     KEYBOARD_IDX = { 
         "forward": 0, "back": 1, "left": 2, "right": 3
+    }
+
+    CAM_VALUE = 0.1
+    CAMERA_VALUE_MAP = {
+        "camera_up":  [CAM_VALUE, 0],
+        "camera_down": [-CAM_VALUE, 0],
+        "camera_l":   [0, -CAM_VALUE],
+        "camera_r":   [0, CAM_VALUE],
+        "camera_ur":  [CAM_VALUE, CAM_VALUE],
+        "camera_ul":  [CAM_VALUE, -CAM_VALUE],
+        "camera_dr":  [-CAM_VALUE, CAM_VALUE],
+        "camera_dl":  [-CAM_VALUE, -CAM_VALUE],
     }
 
     data = []
 
     for action_name in actions_to_test:
         keyboard_condition = [[0, 0, 0, 0] for _ in range(num_samples_per_action)]
+        mouse_condition = [[0,0] for _ in range(num_samples_per_action)] 
 
         for sub_act in base_action:
             if not sub_act in action_name: # 只处理action_name包含的动作
                 continue
-
+            if sub_act in CAMERA_VALUE_MAP:
+                mouse_condition = [CAMERA_VALUE_MAP[sub_act]
+                                   for _ in range(num_samples_per_action)]
             elif sub_act in KEYBOARD_IDX:
                 col = KEYBOARD_IDX[sub_act]
                 for row in keyboard_condition:
@@ -52,5 +83,6 @@ def Bench_actions_universal(num_frames, num_samples_per_action=4):
 
         data.append({
             "keyboard_condition": torch.tensor(keyboard_condition),
+            "mouse_condition": torch.tensor(mouse_condition)
         })
-    return combine_data(data, num_frames, keyboard_dim=4)
+    return combine_data(data, num_frames, keyboard_dim=4, mouse=True)
