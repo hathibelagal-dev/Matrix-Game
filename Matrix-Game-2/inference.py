c9a6ecaaf811eaab2205925c72cf32ac508772e6
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-08-14 18:28:02 +0530

    More cleanup

--- Matrix-Game-2/inference.py
+++ Matrix-Game-2/inference.py
@@ -1,5 +1,3 @@
-import os
-import argparse
 import torch
 import numpy as np
 
--- Matrix-Game-2/utils/wan_wrapper.py
+++ Matrix-Game-2/utils/wan_wrapper.py
@@ -1,68 +1,10 @@
 import types
 from typing import List, Optional
 import torch
-from torch import nn
 from einops import rearrange
 from utils.scheduler import SchedulerInterface, FlowMatchScheduler
-from wan.modules.tokenizers import HuggingfaceTokenizer
-from wan.modules.model import WanModel #, RegisterTokens, GanAttentionBlock
 from wan.modules.vae import _video_vae
-# from wan.modules.t5 import umt5_xxl
 from wan.modules.causal_model import CausalWanModel
-
-
-class WanVAEWrapper(torch.nn.Module): # todo
-    def __init__(self):
-        super().__init__()
-        mean = [
-            -0.7571, -0.7089, -0.9113, 0.1075, -0.1745, 0.9653, -0.1517, 1.5508,
-            0.4134, -0.0715, 0.5517, -0.3632, -0.1922, -0.9497, 0.2503, -0.2921
-        ]
-        std = [
-            2.8184, 1.4541, 2.3275, 2.6558, 1.2196, 1.7708, 2.6052, 2.0743,
-            3.2687, 2.1526, 2.8652, 1.5579, 1.6382, 1.1253, 2.8251, 1.9160
-        ]
-        self.mean = torch.tensor(mean, dtype=torch.float32)
-        self.std = torch.tensor(std, dtype=torch.float32)
-
-        # init model
-        self.model = _video_vae(
-            pretrained_path="skyreels_models/Wan2.1-T2V-1.3B/Wan2.1_VAE.pth",
-            z_dim=16,
-        ).eval().requires_grad_(False)
-
-    def encode_to_latent(self, pixel: torch.Tensor) -> torch.Tensor:
-        # pixel: [batch_size, num_channels, num_frames, height, width]
-        device, dtype = pixel.device, pixel.dtype
-        scale = [self.mean.to(device=device, dtype=dtype),
-                 1.0 / self.std.to(device=device, dtype=dtype)]
-
-        output = [
-            self.model.encode(u.unsqueeze(0), scale).float().squeeze(0)
-            for u in pixel
-        ]
-        output = torch.stack(output, dim=0)
-        return output
-
-    def decode_to_pixel(self, latent: torch.Tensor, use_cache: bool = False) -> torch.Tensor:
-        if use_cache:
-            assert latent.shape[0] == 1, "Batch size must be 1 when using cache"
-
-        device, dtype = latent.device, latent.dtype
-        scale = [self.mean.to(device=device, dtype=dtype),
-                 1.0 / self.std.to(device=device, dtype=dtype)]
-
-        if use_cache:
-            decode_function = self.model.cached_decode
-        else:
-            decode_function = self.model.decode
-
-        output = []
-        for u in zs:
-            output.append(decode_function(u.unsqueeze(0), scale).float().clamp_(-1, 1).squeeze(0))
-        output = torch.stack(output, dim=0)
-        return output
-
 
 class WanDiffusionWrapper(torch.nn.Module): 
     def __init__(
